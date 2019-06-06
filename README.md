5f8f15400584f14e88c439deee19d44cb46a9490
Yannis <yannis.samba@gmail.com>
2019-06-06 16:45:17 +0200

    add script.sh

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Jaime_les_coquillages
+
+Contient un fichier script shell avec les commandes.
---
+++ script.sh
@@ -0,0 +1,13 @@
+
+
+# exercice 1
+mkdir Jaime_le_shell;cd Jaime_le_shell
+mkdir test{{2..4},6}; chmod 755 test{{2..4},6}
+touch test{1,5}; chmod 644 test{1,5}
+
+#exercice 2
+mkdir le_shell_cest_trop_cool 
+
+#exercice3
+mv test{1,5} le_shell_cest_trop_cool
+cp -rf test{2,6} le_shell_cest_trop_cool
